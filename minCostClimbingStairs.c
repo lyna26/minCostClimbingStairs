e6aa6a76b3e61add727f2ac040871639473d4305
Lyna <40266991+lyna26@users.noreply.github.com>
2023-10-13 22:15:24 +0200

    Update minCostClimbingStairs.c
    handle memory issue

--- minCostClimbingStairs.c
+++ minCostClimbingStairs.c
@@ -1,17 +1,19 @@
 int minCostClimbingStairs(int *cost, int n){
-  if (n <= 2){
+  	// If the size of the array  is 1, we simply return the cost of the first step. If the size is 2, we return the minimum of the costs of the first two steps.
+	if (n <= 2){
 		return (n == 1 ? cost[0] : fmin(cost[0], cost[1]));
 	}
 	
+	//minCost of the same size as cost to store the minimum cost to reach each step of the staircase.
 	int* dp = (int*) malloc(sizeof(int) * n);
 	dp[0]= cost[0];
 	dp[1] = cost[1];
-  
+	
 	for (int i = 2; i < n; i++){
 		dp[i] = cost[i] + fmin(dp[i - 1], dp[i - 2]);
 	}
-  
+	
 	int minCost = fmin(dp[n - 1], dp[n - 2]);
-  
-  return minCost;
+	free(dp);
+	return minCost;
 }
